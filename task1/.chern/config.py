1124e09ef65f75e144f0c211d2a432c9c2e3e11b
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-04-01 17:06:00 +0800

    Impress: f5ca86a528154b4c947dec6f55e8a9b3

--- task1/.chern/config.py
+++ task1/.chern/config.py
@@ -1,7 +1,7 @@
-impression=u'71f92505ab3447a5a3d7f165af30c60a'
-impressions=['38730967f4384bdf8404aa8b605853c2', 'd2ebf5417eb54b5abe3f9e7b8fe7c0a0', '71f92505ab3447a5a3d7f165af30c60a']
+impression=u'f5ca86a528154b4c947dec6f55e8a9b3'
+impressions=['38730967f4384bdf8404aa8b605853c2', 'd2ebf5417eb54b5abe3f9e7b8fe7c0a0', '71f92505ab3447a5a3d7f165af30c60a', 'f5ca86a528154b4c947dec6f55e8a9b3']
 object_type=u'task'
 output_md5=u'd41d8cd98f00b204e9800998ecf8427e'
 output_md5s={'38730967f4384bdf8404aa8b605853c2': 'd41d8cd98f00b204e9800998ecf8427e'}
-pred_impression=['e1eff7a2e0234e8b982c5724b0b88456']
+pred_impression=['10d99a921c3e407490791cf9635082de']
 predecessors=['algorithm']

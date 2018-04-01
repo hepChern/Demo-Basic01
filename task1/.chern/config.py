ade2c7aaa8e49187560570fcb1a44fb0eb5a2342
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-04-01 17:01:50 +0800

    Impress: 71f92505ab3447a5a3d7f165af30c60a

--- task1/.chern/config.py
+++ task1/.chern/config.py
@@ -1,7 +1,7 @@
-impression=u'd2ebf5417eb54b5abe3f9e7b8fe7c0a0'
-impressions=['38730967f4384bdf8404aa8b605853c2', 'd2ebf5417eb54b5abe3f9e7b8fe7c0a0']
+impression=u'71f92505ab3447a5a3d7f165af30c60a'
+impressions=['38730967f4384bdf8404aa8b605853c2', 'd2ebf5417eb54b5abe3f9e7b8fe7c0a0', '71f92505ab3447a5a3d7f165af30c60a']
 object_type=u'task'
 output_md5=u'd41d8cd98f00b204e9800998ecf8427e'
 output_md5s={'38730967f4384bdf8404aa8b605853c2': 'd41d8cd98f00b204e9800998ecf8427e'}
-pred_impression=['8784af2ce58b409a99f07fc1a4a8bda9']
+pred_impression=['e1eff7a2e0234e8b982c5724b0b88456']
 predecessors=['algorithm']
--- task1/.chern/parameters.py
+++ task1/.chern/parameters.py
@@ -1,4 +1,5 @@
+NEvents=u'10000'
 mean=u'1'
-parameters=['mean', 'sigma1', 'sigma2']
+parameters=['mean', 'sigma1', 'sigma2', 'NEvents']
 sigma1=u'1'
 sigma2=u'3'

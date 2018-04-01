2bef5589ff3a51758313b6c6db6a76284cae5130
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-04-01 17:08:33 +0800

    Impress: f9406599b8ce40f38adfe443af1c5302

--- task1/.chern/parameters.py
+++ task1/.chern/parameters.py
@@ -1,5 +1,6 @@
-NEvents=u'10000'
+NEvents=None
+Nevents=u'10000'
 mean=u'1'
-parameters=['mean', 'sigma1', 'sigma2', 'NEvents']
+parameters=['mean', 'sigma1', 'sigma2', 'Nevents']
 sigma1=u'1'
 sigma2=u'3'

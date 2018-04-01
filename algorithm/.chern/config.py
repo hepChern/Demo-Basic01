86fc6992a6d4e96dee84c6a802ce9ad377009c9a
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-04-01 17:11:54 +0800

    Impress: 0a0f9a60d2fe4eb093bfdca2a8308c1b

--- algorithm/.chern/config.py
+++ algorithm/.chern/config.py
@@ -1,6 +1,6 @@
-impression=u'10d99a921c3e407490791cf9635082de'
-impressions=['5b7839b462af43fe8ab05ffdbaeffa84', '718ddaa099ad4ec2984359d98e35481a', '8784af2ce58b409a99f07fc1a4a8bda9', 'e1eff7a2e0234e8b982c5724b0b88456', '10d99a921c3e407490791cf9635082de']
+impression=u'0a0f9a60d2fe4eb093bfdca2a8308c1b'
+impressions=['5b7839b462af43fe8ab05ffdbaeffa84', '718ddaa099ad4ec2984359d98e35481a', '8784af2ce58b409a99f07fc1a4a8bda9', 'e1eff7a2e0234e8b982c5724b0b88456', '10d99a921c3e407490791cf9635082de', '0a0f9a60d2fe4eb093bfdca2a8308c1b']
 main_file=u'main.py'
 object_type=u'algorithm'
 pred_impression=[]
-successors=['task1']
+successors=['task1', 'task2']

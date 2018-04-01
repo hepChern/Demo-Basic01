0f73c7f4d7affd30287a3f718ae7294790030f03
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-04-01 17:11:02 +0800

    Impress: 0c4d6eff19c847969f0efde383c4ec70

--- task2/.chern/config.py
+++ task2/.chern/config.py
@@ -1,1 +1,4 @@
-object_type = "task"
+impression=u'0c4d6eff19c847969f0efde383c4ec70'
+object_type=u'task'
+pred_impression=[]
+impressions=['0c4d6eff19c847969f0efde383c4ec70']
---
+++ task2/.chern/parameters.py
@@ -0,0 +1,5 @@
+Nevents=u'1000000'
+mean=u'1'
+parameters=['mean', 'sigma1', 'sigma2', 'Nevents']
+sigma1=u'1.5'
+sigma2=u'3'

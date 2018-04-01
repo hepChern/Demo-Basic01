2339ba0f87f177a0dbefd7354cee99272c76a027
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-04-01 16:42:20 +0800

    Impress: 5b7839b462af43fe8ab05ffdbaeffa84

--- algorithm/.chern/config.py
+++ algorithm/.chern/config.py
@@ -1,2 +1,5 @@
-object_type = "algorithm"
-main_file = "main.py"
+impression=u'5b7839b462af43fe8ab05ffdbaeffa84'
+main_file=u'main.py'
+object_type=u'algorithm'
+pred_impression=[]
+impressions=['5b7839b462af43fe8ab05ffdbaeffa84']

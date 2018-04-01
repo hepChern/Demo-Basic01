f020b878ac4656ddf87386bf12936b726f5e3687
Mingrui Zhao <mingrui.zhao@mail.labz0.org>
2018-04-01 16:53:47 +0800

    Impress: 38730967f4384bdf8404aa8b605853c2 + record output_md5

--- task1/.chern/config.py
+++ task1/.chern/config.py
@@ -1,5 +1,7 @@
 impression=u'38730967f4384bdf8404aa8b605853c2'
+impressions=['38730967f4384bdf8404aa8b605853c2']
 object_type=u'task'
+output_md5=u'd41d8cd98f00b204e9800998ecf8427e'
 pred_impression=['718ddaa099ad4ec2984359d98e35481a']
 predecessors=['algorithm']
-impressions=['38730967f4384bdf8404aa8b605853c2']
+output_md5s={'38730967f4384bdf8404aa8b605853c2': 'd41d8cd98f00b204e9800998ecf8427e'}
